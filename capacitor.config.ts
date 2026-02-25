d87322c21e8e3207b52b810da647e34afc817d7b
litospayaso <angel.jesuita@gmail.com>
2026-02-25 20:57:44 +0100

    adding capacitor to the project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.brote.app',
+  appName: 'Brote',
+  webDir: 'gh-pages'
+};
+
+export default config;
